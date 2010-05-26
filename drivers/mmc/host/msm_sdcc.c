60d2ad1442971d6d08a6311e7525ea492cc89c9d
Dmitry Shmidt <dimitrysh@google.com>
2010-05-26 12:55:37 -0700

    drivers: mmc: msm: Change max frequency to 49152000
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -60,7 +60,7 @@
 #define SQN_BUSCLK_TIMEOUT (5 * HZ)
 
 static unsigned int msmsdcc_fmin = 144000;
-static unsigned int msmsdcc_fmax = 50000000;
+static unsigned int msmsdcc_fmax = 49152000;
 static unsigned int msmsdcc_4bit = 1;
 static unsigned int msmsdcc_pwrsave = 1;
 static unsigned int msmsdcc_piopoll = 1;
