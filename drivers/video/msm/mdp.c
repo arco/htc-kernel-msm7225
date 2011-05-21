7f836bcf42bad22a9972b46277d81200e79f2d30
Arne Coucheron <arco68@gmail.com>
2011-05-21 19:08:10 +0200

    VIDEO: MSM: Fix memory leak in mdp_probe
    Fix for not releasing mdp when failing to get clk
    
    Change-Id: I6973f6d4db7ef70d06301717525e56a6619bf177

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -1231,6 +1231,7 @@ int mdp_probe(struct platform_device *pdev)
 
 	mdp->clk = clk_get(&pdev->dev, "mdp_clk");
 	if (IS_ERR(mdp->clk)) {
+		kfree(mdp);
 		PR_DISP_INFO("mdp: failed to get mdp clk");
 		ret = PTR_ERR(mdp->clk);
 		goto error_get_mdp_clk;
