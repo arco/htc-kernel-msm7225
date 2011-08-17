4e4ab3510e63a9e53ccc8cbe700b676daafdc02a
Arne Coucheron <arco68@gmail.com>
2011-08-17 05:51:03 +0200

    msm: mdp: Fix ifdef check to avoid compile error

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -524,7 +524,7 @@ static void mdp_dma_to_mddi(void *priv, uint32_t addr, uint32_t stride,
 	} else if (mdp->mdp_dev.color_format == MSM_MDP_OUT_IF_FMT_RGB666) {
 		dma2_cfg |= DMA_DSTC0G_6BITS | DMA_DSTC1B_6BITS | DMA_DSTC2R_6BITS;
 		video_packet_parameter = MDDI_VDO_PACKET_DESC_RGB666;
-#ifndef CONFIG_MACH_BUZZ
+#if !defined(CONFIG_ARCH_MSM7X00A) && !defined(CONFIG_ARCH_MSM7225)
 	} else {
 		dma2_cfg |= DMA_IBUF_FORMAT_XRGB8888;
 		dma2_cfg |= DMA_DSTC0G_8BITS | DMA_DSTC1B_8BITS | DMA_DSTC2R_8BITS;
