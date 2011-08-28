36382c157e0cc251ded869a3949d7ebf837fa990
Arne Coucheron <arco68@gmail.com>
2011-08-28 07:43:29 +0200

    Revert "PM: Allow wakeup events to abort freezing of tasks"
    This reverts commit fab9c5eab503a450b54d0d0b55b748ed0ee9b70d.

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -39,7 +39,7 @@ static int try_to_freeze_tasks(bool sig_only)
 	struct timeval start, end;
 	u64 elapsed_csecs64;
 	unsigned int elapsed_csecs;
-	bool wakeup = false;
+	unsigned int wakeup = 0;
 
 	do_gettimeofday(&start);
 
@@ -78,11 +78,6 @@ static int try_to_freeze_tasks(bool sig_only)
 		if (!todo || time_after(jiffies, end_time))
 			break;
 
-		if (!pm_check_wakeup_events()) {
-			wakeup = true;
-			break;
-		}
-
 		/*
 		 * We need to retry, but first give the freezing tasks some
 		 * time to enter the regrigerator.
@@ -108,15 +103,15 @@ static int try_to_freeze_tasks(bool sig_only)
 		}
 		else {
 			printk("\n");
-			printk(KERN_ERR "Freezing of tasks %s after %d.%02d seconds "
+			printk(KERN_ERR "Freezing of tasks failed after %d.%02d seconds "
 					"(%d tasks refusing to freeze):\n",
-					wakeup ? "aborted" : "failed",
 					elapsed_csecs / 100, elapsed_csecs % 100, todo);
 		}
 		read_lock(&tasklist_lock);
 		do_each_thread(g, p) {
 			task_lock(p);
-			if (!wakeup && freezing(p) && !freezer_should_skip(p))
+			if (freezing(p) && !freezer_should_skip(p) &&
+				elapsed_csecs > 100)
 				sched_show_task(p);
 			cancel_freezing(p);
 			task_unlock(p);
